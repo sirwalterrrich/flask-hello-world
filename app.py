fba9b71054f91070ba76ce7076cbcbcf282b0692
sirwalterrrich <sirwalterrich@gmail.com>
2024-10-20 14:26:07 -0400

    updated Date-Time

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f'Hello, World! Current date and time: {current_time}'
